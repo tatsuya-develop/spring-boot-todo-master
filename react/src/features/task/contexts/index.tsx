1a10ef815d4f6e0e478344f4dff186d84bd69404
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-09-01 15:43:25 +0900

    add: reactプロジェクトの追加

---
+++ react/src/features/task/contexts/index.tsx
@@ -0,0 +1,49 @@
+import type Task from "@/features/task/models/task";
+import { createContext, useState } from "react";
+
+export interface TaskContextType {
+  tasks: Task[];
+  selectedTask?: Task;
+  setTasks: (tasks: Task[]) => void;
+  upsertTasks: (task: Task) => void;
+  deleteTask: (id: number) => void;
+  selectTask: (task: Task) => void;
+  resetSelectedTask: () => void;
+}
+
+export const TaskContext = createContext<TaskContextType | undefined>(
+  undefined,
+);
+
+export const TaskProvider = ({ children }: { children: React.ReactNode }) => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const upsertTasks = (task: Task) => {
+    if (tasks.some((t) => t.id === task.id)) {
+      // 更新
+      setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    } else {
+      // 追加
+      setTasks([...tasks, task]);
+    }
+  };
+  const deleteTask = (id: number) => setTasks(tasks.filter((task) => task.id !== id));
+  const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+  const selectTask = (task?: Task) => setSelectedTask(task);
+  const resetSelectedTask = () => setSelectedTask(undefined);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        selectedTask,
+        setTasks,
+        upsertTasks,
+        deleteTask,
+        selectTask,
+        resetSelectedTask,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
+};
