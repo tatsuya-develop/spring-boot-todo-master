5c5bf8e3ce06ee45b497c5f2e55306b945ae53de
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-09-19 07:53:29 +0900

    add: Project一覧取得用のController を本実装

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectListController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectListController.java
@@ -6,19 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.entity.Project;
+import com.example.todo.service.projects.ProjectListService;
 
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectListController {
 
-  public ProjectListController() {
-    // Constructor
+  private final ProjectListService projectListService;
+
+  public ProjectListController(ProjectListService projectListService) {
+    this.projectListService = projectListService;
   }
 
   @GetMapping
   public ResponseEntity<List<Project>> invoke() {
-    // TODO: ダミーデータでの仮実装のため、本修正必須。
-    List<Project> projects = List.of(new Project());
+    List<Project> projects = this.projectListService.invoke();
 
     return ResponseEntity.ok(projects);
   }
