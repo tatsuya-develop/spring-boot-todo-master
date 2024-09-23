a3cbb04f91807a43d9a78b3ca5f9bb2048baf4f1
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-09-23 11:18:58 +0900

    update: TaskCreateService で、name, priorityをセットしDB保存可能にした

--- spring/src/main/java/com/example/todo/service/tasks/TaskCreateService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskCreateService.java
@@ -4,17 +4,26 @@ import org.springframework.stereotype.Service;
 import com.example.todo.dto.request.tasks.TaskCreateRequest;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
 import com.example.todo.entity.Task;
+import com.example.todo.repository.TaskRepository;
 
 @Service
 public class TaskCreateService {
 
-  public TaskCreateService() {
+  private final TaskRepository taskRepository;
+
+  public TaskCreateService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
 
   }
 
   public TaskBaseResponse invoke(TaskCreateRequest request) {
     Task task = new Task();
 
-    return new TaskBaseResponse(task);
+    task.setName(request.getName());
+    task.setPriority(request.getPriority());
+
+    // TODO: プロジェクトの追加
+
+    return new TaskBaseResponse(this.taskRepository.save(task));
   }
 }
