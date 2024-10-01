2d9c72bb6a2156f329886076ff54b814a32eb435
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-10-02 08:19:00 +0900

    add: Toggle の Controller 実装と Service の仮実装

---
+++ spring/src/main/java/com/example/todo/service/tasks/TaskToggleService.java
@@ -0,0 +1,15 @@
+package com.example.todo.service.tasks;
+
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+
+@Service
+public class TaskToggleService {
+
+  public TaskToggleService() {}
+
+  public TaskBaseResponse invoke(Integer id) {
+    // TODO: 仮実装
+    return null;
+  }
+}
