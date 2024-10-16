6415d6a454ff342c7a06735ea25d5bbca18c3a8a
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-10-17 08:44:43 +0900

    add: Request/Controller/Serviceの仮実装

---
+++ spring/src/main/java/com/example/todo/dto/request/tasks/TaskUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.example.todo.dto.request.tasks;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TaskUpdateRequest {
+
+}
---
+++ spring/src/main/java/com/example/todo/service/tasks/TaskUpdateService.java
@@ -0,0 +1,14 @@
+package com.example.todo.service.tasks;
+
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+
+@Service
+public class TaskUpdateService {
+
+  public TaskUpdateService() {}
+
+  public TaskBaseResponse invoke() {
+    return null;
+  }
+}
